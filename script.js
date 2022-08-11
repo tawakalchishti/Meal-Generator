8fda0ff5d646587863c053ea0aa8f7749c5d8030
tawakalchishti <tawakalchishti9@gmail.com>
2022-08-12 01:06:39 +0500

    errors resolved

---
+++ script.js
@@ -0,0 +1,62 @@
+// DOM Elements
+const search = document.getElementById('search');
+const submit = document.getElementById('submit');
+const generate = document.getElementById('generate');
+const resultsHeading = document.getElementById('results-heading');
+const meals = document.getElementById('meals');
+const selectedMeal = document.getElementById('selected-meal');
+
+// Function to search the meal using the API
+function searchMeal(e) {
+    // Prevent form submission and redirect
+    e.preventDefault();
+    // Get the value from search input field
+    const searchText = search.value;
+    // Check if search input field is empty
+    if (searchText.trim()) {
+        // Fetch data from API
+        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                // Update results heading
+                resultsHeading.innerHTML = `<h2>Search results for ${searchText}</h2>`;
+                // Check if any meals returned from API
+                if (data.meals === null) {
+                resultsHeading.innerHTML = `<h2>No results found for ${searchText}</h2>`;
+                } else {
+                    meals.innerHTML = data.meals.map( meal => `
+                        <div class="meal">
+                            <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+                            <div class="meal-info" data-mealID="${meal.idMeal}">
+                                <h3>${meal.strMeal}</h3>
+                            </div>
+                        </div>
+                    `)
+                    .join('')
+                }
+            });
+        // Clear the search text
+        search.value = '';
+    } else {
+        alert('Please enter search keyword')
+    }
+}
+
+
+// Event Listeners
+// 1.Listen for form Submit
+submit.addEventListener('submit', searchMeal);
+
+// 2 Listen for click on meals
+meals.addEventListener('click', e => {
+    // Find and returned only if clicked on a meal-info div
+    const mealInfo = e.path.find(item => {
+        if (item.classList) {
+            return item.classList.contains('meal-info');
+        } else {
+            return false;
+        }
+    });
+    console.log(mealInfo);
+})
